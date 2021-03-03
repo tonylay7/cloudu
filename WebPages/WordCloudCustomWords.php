c74e04b61dd02fcd8b7b8bf4f20887384da30d1d
s11978tl <tony.lay@student.manchester.ac.uk>
2021-03-03 14:58:13 +0000

    added buttons for custom words

--- WebPages/WordCloudCustomWords.php
+++ WebPages/WordCloudCustomWords.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-ui.min.js"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-exports.min.js"></script>
-    <script src="https://cdn.anychart.com/releases/v8/js/anychart-tag-cloud.min.js"></script>
-    <link href="https://cdn.anychart.com/releases/v8/css/anychart-ui.min.css" type="text/css" rel="stylesheet">
-    <link href="https://cdn.anychart.com/releases/v8/fonts/css/anychart-font.min.css" type="text/css" rel="stylesheet">
-
     <link rel="stylesheet" type="text/css" href="styles.css">
     <meta charset="UTF-8">
-    <title>Word Cloud</title>
-
+    <title>Word Cloud - Add/Remove Words</title>
     <style type="text/css">
       html,
       body,
@@ -58,6 +49,9 @@
         -ms-transform: translate(-50%, -50%);
         transform: translate(-50%, -50%);
       }
+      textarea {
+        resize: none;
+      }
     </style>
   </head>
   <body>
@@ -73,37 +67,15 @@
     </ul>
     <div class="container">
       <div class="center">
-        <a href="WordCloudCustomWords.php" class="myButton">Add/Remove Words</a>  
+        <a href="WordCloud.php" class="myButton">Back</a>  
+        <input class='myButton' type='button' value='Save' onclick='showAnswer()'>
       </div>
     </div>
-    
-  <div id="container"></div>
-    <?php
-      $database_host = "dbhost.cs.man.ac.uk";
-      $database_user = "n00575sm";
-      $database_pass = "Mozzer_2310";
-      $database_name = "2020_comp10120_x6";
-
-      $conn = mysqli_connect($database_host,$database_user,$database_pass,$database_name);
-      
-      if (!$conn){
-        die("connection failed: " . mysqli_connect_error());
-      }
-
-      $sqldiary = "SELECT * FROM diaryentries";
-      $diaryresult = $conn->query($sqldiary);
-      $diaryrow = $diaryresult->fetch_assoc();
-
-      $sqlwords = "SELECT * FROM customwords";
-      $wordsresult = $conn->query($sqlwords);
-      $wordsrow = $wordsresult->fetch_assoc();
-    ?>
-    <script type="text/javascript">
-      var db_text = "<?php echo $diaryrow['grateful_text']; ?>";
-      var db_addwords = "<?php echo $wordsrow['addwords']; ?>";
-      var db_removewords = "<?php echo $wordsrow['removewords']; ?>";
-    </script>
-    <script src="../Scripts/bundle.js"></script>
-
+    <div id="container">
+      <div class="center">
+        <textarea class="textarea" id="addwords" name="addwords" rows="20" cols="50"></textarea>
+        <textarea class="textarea" id="removewords" name="removewords" rows="20" cols="50"></textarea>
+      </div>
+    </div>
   </body>
 </html>
