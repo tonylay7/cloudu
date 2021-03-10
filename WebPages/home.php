554ec01eaf8b20cc1e200be1ad94f929ce058996
n36579gg <germaine.george@student.manchester.ac.uk>
2021-03-10 18:32:32 +0000

    Registration

--- WebPages/home.php
+++ WebPages/home.php
@@ -2,25 +2,56 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
-<html>
-  	
-  	<head>
-  		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="home.css">
-     	<title>Welcome to CLOUDU</title>
-  	</head>
-  	<body>
-  	<header>
- 		<h1 style="line-height: 80px;">Welcome to CLOUDU!</h1>
- 	</header>
-  	<main>
-    <form action="template.html">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<title>Welcome to CLOUDU</title>
+</head>
+
+<body>
+<div class="slideshow-block" style ="width:70%;padding:0;margin:0;height:90%">
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <!-- <img src="img1.jpg" style="width:100%"> -->
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <!-- <img src="img2.jpg" style="width:100%"> -->
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <!-- <img src="img3.jpg" style="width:100%"> -->
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+	</div>
+	<br>
+
+	<!-- The dots/circles -->
+	<div style="text-align:center">
+	  <span class="dot" onclick="currentSlide(1)"></span>
+	  <span class="dot" onclick="currentSlide(2)"></span>
+	  <span class="dot" onclick="currentSlide(3)"></span>
+	</div>
+	</div>
+	<div style="position:absolute; top: 0; right:20px;width:30%; height:90%; padding:0">
+		<form action="template.html" method="post">
   		<table>
 			<tr>
 				<td><label for="username">Username:</label></td>
 				<td><input type="text" name="username" id="username"
-					      value=""
+					      value="" 
 					      placeholder ="Enter username"></td>
 			</tr>
 			<tr>
@@ -32,39 +63,69 @@ session_start();
 		    <br>
 		    <tr>
 		    	<td><input type="submit" value="Login" ></td>
-		    	<td><input type="submit" formaction="register.html" value="Register as New User"></td>
+		    	<td><input type="submit" formaction="register.php" value="Register as New User" method="post"></td>
 		    </tr>
 		</table>
-	</form>
-    </main>
-	<footer>
-		<?php
+		</form>
+	</div>
+</body>
 
-		$curl = curl_init();
-		$URL = "https://zenquotes.io/api/random";
-		curl_setopt($curl, CURLOPT_URL, $URL);
-		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+<script type="text/javascript">
+	var slideIndex = 1;
+	showSlides(slideIndex);
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+	// Next/previous controls
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
 
-		if ($err) {
-			echo "cURL Error #:" . $err;
-		} else {
-		//	echo $response;
-		}
+	// Thumbnail image controls
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
 
-		$response = json_decode($response, true);
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("dot");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
+	}
 
-		$_SESSION["quote"] = $response[0]['q'];
-		$_SESSION["person"] = $response[0]['a'];
+</script>
+<footer>
+	<?php
 
-		echo ("<br>" . $_SESSION["quote"] . "<br> -" . $_SESSION["person"]);
+	$curl = curl_init();
+	$URL = "https://zenquotes.io/api/random";
+	curl_setopt($curl, CURLOPT_URL, $URL);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-		?>
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
 
- 	</footer>
-  </body>
-</html>
+	if ($err) {
+		echo "cURL Error #:" . $err;
+	} else {
+	//	echo $response;
+	}
 
+	$response = json_decode($response, true);
+
+	$_SESSION["quote"] = $response[0]['q'];
+	$_SESSION["person"] = $response[0]['a'];
+
+	echo ("<br>" . $_SESSION["quote"] . "<br> -" . $_SESSION["person"]);
+
+	?>
+
+</footer>
